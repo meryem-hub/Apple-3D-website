6fb745787ef6cdc88903bceb3f5605e36232ac68
Meryem Ebrahim <meryemebrahim.46@gmail.com>
2025-04-14 04:50:28 -0700

    Add responsive Navbar component with logo

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hero() {
+  return (
+   <section className='w-full nav-height bg-black relative'>
+<div className='h-5/6 w-ful flex-center flex-col'>
+<p className='hero=title'>iPhone 15 Pro</p></div>
+   </section>
+  )
+}
+
+export default Hero
